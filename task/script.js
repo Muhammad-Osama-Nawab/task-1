bbcffe80a095c851b0cbd3df69a427234b90af2b
Muhammad-Osama-Nawab <usamanawab@gmail.com>
2024-10-15 18:15:38 +0500

    composition function

--- task/script.js
+++ task/script.js
@@ -1,1 +1,18 @@
-console.log('hello')
+function ready(w)
+{
+  if(w==1)
+  {
+    return w+". Be Ready"
+  }
+}
+function go(w)
+{
+  return w + "- to - go"
+}
+
+function wait(ready,go,w)
+{
+  return go(ready(w))
+}
+let f=wait(ready,go,1)
+console.log(f)
